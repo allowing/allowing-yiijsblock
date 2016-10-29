530d6aec736be2acec2bf04b6f6b6bffcc04e7ca
allowing <ljjgit@qq.com>
2016-10-29 13:50:45 +0800

    完成widget的编写

---
+++ src/allowing-yiijsblock/YiiJsBlockWidget.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace allowing\yiijsblock;
+
+use yii\base\Widget;
+use yii\web\View;
+
+class YiiJsBlock extends Widget
+{
+    public $position;
+
+    public function init()
+    {
+        parent::init();
+
+        if ($this->position === null) {
+            $this->position = View::POS_END;
+        }
+    }
+
+    public function run()
+    {
+        $content = ob_get_clean();
+
+        // <script></script>
+        preg_match('/<script.*?>(.*?)<script>/is', $content, $match);
+
+        $js = isset($match[1]) ? $match[1] : '';
+
+        $this->view->registerJs($js, $this->position);
+    }
+}
